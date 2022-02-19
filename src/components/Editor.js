85ef7ba7610fb09ec4b37f0e27072126c3ee0b68
stefanavra <stefan.avra@gmail.com>
2022-02-19 20:58:25 +0100

    weird

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -2,6 +2,13 @@ import styles from "./Editor.module.css";
 import { useState, useEffect, createElement, useRef } from "react";
 import SongSelect from "./SongSelect";
 import RangeSlider from "./RangeSlider";
+import covers from "./covers/covers";
+
+//create your forceUpdate hook
+function useForceUpdate() {
+    const [value, setValue] = useState(0); // integer state
+    return () => setValue((value) => value + 1); // update the state to force render
+}
 
 export default function Editor() {
     const songOptions = [
@@ -9,15 +16,17 @@ export default function Editor() {
         { value: "b", label: "Song B", image: "low_rider" },
     ];
 
+    const imageOptions = [];
+    const forceUpdate = useForceUpdate();
+
     const [selectedSong, setSelectedSong] = useState({
         value: "a",
         label: "Song A",
         image: "japan",
     });
 
-    const [imgSource, setImgSource] = useState(
-        `/covers/${selectedSong.image}.jpeg`
-    );
+    const [imgSource, setImgSource] = useState();
+    // `/covers/${selectedSong.image}.jpeg`
 
     const canvasRef = useRef(null);
     const imgRef = useRef(null);
@@ -29,23 +38,42 @@ export default function Editor() {
     }, []);
 
     useEffect(() => {
-        setImgSource(`/covers/${selectedSong.image}.jpeg`);
+        // setImgSource(`/covers/${selectedSong.image}.jpeg`);
+        setImgSource(covers.filter((img) => img.includes(selectedSong.image)));
+        console.log(
+            `imgSource is: ${imgSource} and set it to ${selectedSong.image} and ${covers}`
+        );
+    }, [selectedSong]);
+
+    useEffect(() => {
+        // const image = createElement("img");
+        // image.src = imgSource;
+        // imgRef.onload = () => {
+        //     drawImageToCtx();
+        // };
+        drawImageToCtx();
+    }, [imgSource]);
+
+    function drawImageToCtx() {
+        forceUpdate();
+
+        console.log(`drawing image to ctx`);
         const canvas = canvasRef.current;
         const ctx = canvas.getContext("2d");
+        const imageToDraw = imgRef.current;
 
         ctx.drawImage(
-            imgRef.current,
+            imageToDraw,
             0,
             0,
-            imgRef.current.width,
-            imgRef.current.height,
+            imageToDraw.width,
+            imageToDraw.height,
             0,
             0,
             canvas.width,
             canvas.height
         );
-        console.log(`drawing ${selectedSong.image}`);
-    }, [selectedSong, imgRef]);
+    }
 
     function handleChangeSong(selectedSong) {
         setSelectedSong(
@@ -71,6 +99,7 @@ export default function Editor() {
                 src={imgSource}
                 alt="cover art"
                 ref={imgRef}
+                // onChange={}
             />
             <div className={styles.settings}>
                 <SongSelect
@@ -99,3 +128,7 @@ export default function Editor() {
         </div>
     );
 }
+
+const Image = (props) => (
+    <img className={styles.hidden} src={props.imgSource} alt="cover art" />
+);
---
+++ src/components/covers/covers.js
@@ -0,0 +1,5 @@
+import japan from "./japan.jpeg";
+import low_rider from "./low_rider.jpeg";
+
+const covers = [japan, low_rider];
+export default covers;
